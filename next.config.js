5abab1885220f2a90c48f31a1eeaf66296583ab5
natanjs01 <natanjs01@users.noreply.github.com>
2026-04-14 18:37:14 -0300

    Configurar deploy do GitHub Pages para Next export

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+const repoName = 'projecoes_grupo_lider';
+
 const nextConfig = {
+  output: 'export',
+  basePath: isGithubActions ? `/${repoName}` : '',
+  assetPrefix: isGithubActions ? `/${repoName}/` : '',
+  trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
